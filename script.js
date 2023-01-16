e8f8c584845b03172ee9059b38cd8187d47ab573
karynnemoreira <karynnemoreira@yahoo.com.br>
2023-01-16 19:37:43 -0300

    Declarando variáveis, Tipos de dados, Comparações, Operadores de comparação, Estrutura condicional.

---
+++ script.js
@@ -0,0 +1,82 @@
+//console.log(3 > 2) //true, pq 3 é maior 2 
+//console.log (3 < 2) //falso, pq 3 não é menor que 2
+
+
+// = atribuição de tipo
+let number = 2
+let numero = 2
+//console.log(number, numero)
+
+// == verificação de valor
+
+//console.log(numero == 5) //false, pq o valor que está guardado não é 5. 
+//console.log (numero == 2) //true, pq o valor que está guardado é 2.
+
+// === verificação de tipo e valor
+//console.log(number === 2) //true, pq o valor e o tipo são verdadeiros
+//console.log(numero === "família")
+
+// != verifica se os valores comparados são diferentes
+
+let verificar = "feijão"
+
+//console.log(verificar != "feijão") //false, pq é mentira que eles são diferentes.
+//console.log(verificar != "arroz") //true, pq é verdade que eles são diferentes.
+
+// !== verifica se o valor e o tipo são diferentes
+
+let diferente = `Vou ficar rica`
+
+//console.log (diferente !== 472) //true, pq o valor e o tipo são diferentes
+
+//console.log (diferente !== "Se Deus quiser")//true, pq ela é string mas o valor guardado não é o mesmo.
+
+//console.log(diferente !== 'Vou ficar rica')//false, pq o valor e o tipo são os mesmos.
+
+
+//  > maior que; < menor que; <= menor igual; >= maior igual
+
+let n1 = 7
+let n2 = 15
+
+//console.log(n2 > 54)//false, pq 15 é menor que 54
+//console.log(n1 < -2)//false, pq 7 é maior que -2
+//console.log (n2 != 23) //true, pq 15 é diferente de 23
+//console.log (n1 === "azul")//false, pq é diferente o meu tipo e valor
+
+
+// ------Estrutura If 
+
+// if(condição){
+//     retorno do if
+//     console.log()
+// }
+
+
+// let idade = 13
+
+// if (idade >= 18){
+//     console.log("Pode assistir filme de terror")
+// }else{
+//     console.log("Vai assistir barbie")
+// }
+
+
+let primos = 17
+let nome = "Beyonce"
+const nascimento = 1998
+let $numeroDaSorte = 56
+let _idade = 17
+
+// if(primos == $numeroDaSorte){
+//     console.log("Temos o mesmo valor")
+// } else{
+//     console.log("Não temos o mesmo valor")
+// }
+
+// if(primos === _idade){
+//     console.log("Temos o mesmo valor e tipo")
+// } else{
+//     console.log("Não temos o mesmo valor e nem tipo")
+// }
+
